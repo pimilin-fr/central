43828193f95f810380ba3dbbb40dc770244c67f7
pimlin <pimlin@Pimpo>
2026-09-10 19:44:06 +0200

    FIXES

--- src/Controller/AdresseController.php
+++ src/Controller/AdresseController.php
@@ -59,30 +59,8 @@ final class AdresseController extends AbstractController {
     }
 
     #[Route('/edit/{id}', name: 'app_adresse_edit', methods: ['GET', 'POST'])]
-    public function edit(Adresse $adresse, Request $request, EntityManagerInterface $em): Response {
-
-        $form = $this->createForm(AdresseFormType::class, $adresse);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em->flush();
-
-            $this->addFlash('success', 'Adresse modifié avec succès');
-
-            return $this->redirectToRoute(
-                            'app_adresse_show',
-                            [
-                                'id' => $adresse->getId(),
-                                'tab' => 'edit',
-                            ]
-                    );
-        }
-
-        return $this->render('adresse/_form.html.twig', [
-                    'form' => $form->createView(),
-                    'portefeuille' => $adresse,
-        ]);
+    public function edit(Adresse $adresse, Request $request, EntityManagerInterface $em, Geocoder $geocoder, AdresseMapBuilder $adresseMapBuilder): Response {
+        return $this->handleForm($request, $adresse, $em, $geocoder, $adresseMapBuilder, 'Adresse modifiée avec succès');
     }
 
     #[Route('/show/{id}', name: 'app_adresse_show', methods: ['GET', 'POST'])]
